9bbd260e7a149bec4a288ef3a77692ebbc4c222b
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-10 01:14:58 +0530

    adds basic UI for the application

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/models/category.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -8,8 +11,59 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Native mobile development course',
+        amount: 19.99,
+        time: DateTime.now(),
+        category: ExpenseCategory.work),
+    Expense(
+        title: 'Native android development course',
+        amount: 5893.99,
+        time: DateTime.now(),
+        category: ExpenseCategory.food),
+    Expense(
+        title: 'Native tv development course',
+        amount: 39.99,
+        time: DateTime.now(),
+        category: ExpenseCategory.lesiure),
+    Expense(
+        title: 'Native tv development course',
+        amount: 39.99,
+        time: DateTime.now(),
+        category: ExpenseCategory.travel),
+    Expense(
+        title: 'Native tv development course',
+        amount: 39.99,
+        time: DateTime.now(),
+        category: ExpenseCategory.lesiure),
+    Expense(
+        title: 'Native tv development course',
+        amount: 39.99,
+        time: DateTime.now(),
+        category: ExpenseCategory.lesiure),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter ExpenseTracker"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text("The chart"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/expense_item.dart
@@ -0,0 +1,45 @@
+import 'package:expense_tracker/models/category.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expenseItem});
+
+  final Expense expenseItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            Text(expenseItem.title),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text(
+                  '\$${expenseItem.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expenseItem.category]),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      expenseItem.formattedDate,
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
