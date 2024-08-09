e824284843730e4b210cabb4462c2113c0b70737
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-09 23:19:19 +0530

    add: category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
@@ -8,10 +9,12 @@ class Expense {
     required this.title,
     required this.amount,
     required this.time,
+    required this.category,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime time;
+  final Category category;
 }
