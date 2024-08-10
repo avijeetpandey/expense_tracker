2381f9ea5740af88adb1af2e94306d49c5484f92
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2024-08-10 20:55:45 +0530

    add: chart widget (#4)

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/category.dart';
+import 'package:expense_tracker/widgets/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,12 @@ class _ExpensesState extends State<Expenses> {
         title: "Test Expense",
         amount: 23,
         time: DateTime.now(),
-        category: ExpenseCategory.food)
+        category: ExpenseCategory.food),
+    Expense(
+        title: "Mobile",
+        amount: 203,
+        time: DateTime.now(),
+        category: ExpenseCategory.work),
   ];
 
   void _openAddExpenseOverlay() {
@@ -85,7 +91,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("The chart"),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           )
