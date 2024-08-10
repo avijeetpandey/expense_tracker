96f079a8ab7e593cf35fba4cd1a8821a0dfdc1d3
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-10 14:01:21 +0530

    add: theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/utils/theme_data.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+      theme: themeData,
+      home: const Expenses(),
     ),
   );
 }
