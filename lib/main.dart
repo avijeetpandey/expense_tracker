72afbba88952129d155e74590ea806f4ba30bf08
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2024-08-10 14:23:37 +0530

    adds theming to the flutter application (#3)
    * add: theme data
    
    * add: dark theme and expense bucket

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/utils/theme_data.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+      theme: themeData,
+      darkTheme: darkThemeData,
+      themeMode: ThemeMode.system,
+      home: const Expenses(),
     ),
   );
 }
