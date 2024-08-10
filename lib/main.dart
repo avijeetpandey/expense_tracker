bf3567574913ff6cb0ce1716a7d486885c6ec2e2
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-10 20:58:46 +0530

    lock device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:expense_tracker/utils/theme_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      theme: themeData,
-      darkTheme: darkThemeData,
-      themeMode: ThemeMode.system,
-      home: const Expenses(),
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  ).then((fn) {
+    runApp(
+      MaterialApp(
+        theme: themeData,
+        darkTheme: darkThemeData,
+        themeMode: ThemeMode.system,
+        home: const Expenses(),
+      ),
+    );
+  });
 }
